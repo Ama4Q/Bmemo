01aacac90c37b56a046fecc9ce25056212f2b5c2
AMA <77095260@qq.com>
2016-11-23 18:45:33 +0800

    commit again

--- Bmemo/Bmemo/BmmMainTableViewController.swift
+++ Bmemo/Bmemo/BmmMainTableViewController.swift
@@ -34,7 +34,7 @@ class BmmMainTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 1
+        return 10
     }
 
     
