88b1038eed6ee8f923905656a9006f7752c13883
AMA <77095260@qq.com>
2016-11-29 19:36:28 +0800

    add detailView

---
+++ Bmemo/Bmemo/BmmHeaderView.swift
@@ -0,0 +1,129 @@
+//
+//  BmmHeaderView.swift
+//  Bmemo
+//
+//  Created by Ama on 11/29/16.
+//  Copyright © 2016 Ama. All rights reserved.
+//
+
+import UIKit
+import RxCocoa
+import RxSwift
+
+typealias ClickHeaderClosure = () -> ()
+
+open class BmmHeaderView: UIView {
+    
+    let disposeBag = DisposeBag()
+
+    fileprivate let kContentOffset = "contentOffset"
+    fileprivate var clickHeaderClosure: ClickHeaderClosure?
+    
+    
+    open var photo: UIImage? = nil {
+        willSet {
+            headerBtn?.setBackgroundImage(newValue, for: .normal)
+            headerBtn?.setBackgroundImage(newValue, for: .highlighted)
+        }
+    }
+    open var boardColor: UIColor? = nil {
+        willSet {
+            headerBtn?.layer.borderColor = newValue?.cgColor
+        }
+    }
+    
+    // MARK: - Rxsiwft scrollView
+    
+    fileprivate var headerBtn: UIButton? {
+        willSet {
+            newValue?
+                .rx
+                .controlEvent(.touchUpInside)
+                .subscribe({ [weak self] _ in
+                    self?.clickHeaderClosure!()
+                    })
+                .addDisposableTo(disposeBag)
+        }
+    }
+    
+    fileprivate var scrollView: UIScrollView? {
+        willSet {
+            newValue?
+                .rx
+                .observe(CGPoint.self, kContentOffset)
+                .subscribe(onNext: { [weak self] (point) in
+                    guard let offsety = point?.y else { return }
+                    var scale: CGFloat = 1.0
+                    if offsety < 0 { scale = min(1.2, 1 - offsety / 300) } else
+                        if offsety > 0 { scale = max(0.55, 1 - offsety / 300) }
+                    
+                    self?
+                        .headerBtn?
+                        .transform = CGAffineTransform(scaleX: scale, y: scale)
+                    self?
+                        .headerBtn?
+                        .frame
+                        .origin
+                        .y = 15
+                    })
+                .addDisposableTo(disposeBag)
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        UIInitial()
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    deinit {
+        debugPrint("BmmHeaderView - deinit")
+    }
+
+}
+
+
+// MARK: - UI
+extension BmmHeaderView {
+    fileprivate func UIInitial() {
+        let kWidth = 63
+        
+        frame = CGRect(x: 0, y: 0, width: kWidth, height: kWidth)
+        
+        headerBtn = UIButton.customBtn(rect: frame)
+        addSubview(headerBtn!)
+    }
+}
+
+// MARK: - open
+extension BmmHeaderView {
+    func reloadSizeWithScrollView(scrollView: UIScrollView) {
+        self.scrollView = scrollView
+    }
+    
+    func handleClickActionWithClosure(closure: @escaping ClickHeaderClosure) {
+        clickHeaderClosure = closure
+        isUserInteractionEnabled = true
+    }
+}
+
+// MARK: - extension buttion
+extension UIButton {
+    class func customBtn(rect: CGRect) -> UIButton {
+        
+        let btn = UIButton(type: .custom)
+        btn.frame = rect
+        
+        btn.backgroundColor = UIColor.clear
+        btn.layer.borderWidth = 0.5
+        btn.layer.cornerRadius = 8
+        btn.layer.borderColor = UIColor.white.withAlphaComponent(0.5).cgColor
+        btn.clipsToBounds = true
+        
+        return btn
+    }
+}
