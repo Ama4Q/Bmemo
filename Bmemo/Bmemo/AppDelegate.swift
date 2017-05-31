67e896afd99df7623235ac31692b946c27919088
zQiu <qiuzhen1@xiaoniu66.com>
2017-05-31 17:15:59 +0800

    Cmt: 增加窗口调试私有方法。

--- Bmemo/Bmemo/AppDelegate.swift
+++ Bmemo/Bmemo/AppDelegate.swift
@@ -16,6 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // private method for debugging application on window.
+        let overlayClass = NSClassFromString("UIDebuggingInformationOverlay") as? UIWindow.Type
+        _ = overlayClass?.perform(NSSelectorFromString("prepareDebuggingOverlay"))
+        
         return true
     }
 
